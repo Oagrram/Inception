01074a75d69faa312a9dddd62fe824f8e052e12f
Othmane Agrram <oagrram@e2r8p9.1337.ma>
2022-10-03 16:25:08 +0100

    finish mariadb container

--- wordpress/setup.sh
+++ wordpress/setup.sh
@@ -1,4 +1,4 @@
-mkdir /var/www/wordpress/ && cd /var/www/wordpress/
+mkdir -p /var/www/wordpress/ && cd /var/www/wordpress/
 wp core download --allow-root
 wp core config --path=/var/www/wordpress --dbname=wordpress_db --dbuser=otman --dbpass=otman --dbhost=mariadb --allow-root
 wp core install --url=oagrram.42.fr --title="ZABOB LIL AKHBAR" --admin_name=otman --admin_password=otman --admin_email=otman@gmail.com --allow-root
