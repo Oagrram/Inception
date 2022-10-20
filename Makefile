35ed60bf0991dd71841f9d4182a792788b8fc042
Othmane Agrram <oagrram@e2r11p4.1337.ma>
2022-10-20 19:01:18 +0100

    need folder structer

---
+++ Makefile
@@ -0,0 +1,20 @@
+NAME = inception
+
+all: prune reload
+
+linux:
+	@ echo "127.0.0.1 oagrram.42.fr" >> /etc/hosts
+	
+stop:
+	@ docker-compose -f srcs/docker-compose.yml down
+
+clean: stop
+	@ rm -rf ~/Desktop/inception
+
+prune: clean
+	@ docker system prune -f
+
+reload: 
+	@ docker-compose -f srcs/docker-compose.yml up --build	
+
+.PHONY: linux stop clean prune reload all
--- srcs/requirements/mariadb/Setup_Database.sh
+++ srcs/requirements/mariadb/Setup_Database.sh
@@ -1,37 +1,27 @@
-# INSTALL MARIADB SERVER
 
 apt install -y mariadb-server
 
-# CONFIG MARIADB BIND ADRESS
-
 sed -i 's+bind-address            = 127.0.0.1+bind-address= 0.0.0.0+g' /etc/mysql/mariadb.conf.d/50-server.cnf 
 
-# RESTART MYSQL SERVICE
+if [ ! -d /var/lib/mysql/$WP_DATABASE_NAME ]; then
 
-service mysql start
+    service mysql start
 
-# CREATE ADMIN USER 
+    echo  "\e[1;32m----------------MARIADB CREATE USER----------------\e[0m"
+    echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
+    echo "GRANT ALL PRIVILEGES ON * . * TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
+    echo "FLUSH PRIVILEGES" | mysql -u root
+    
+    echo  "\e[1;32m----------------MARIADB CREATE DATABASE----------------\e[0m"
+    echo "CREATE DATABASE ${WP_DATABASE_NAME};" | mysql -u root
+    echo "FLUSH PRIVILEGES" | mysql -u root
 
-echo "CREATE USER 'oagrram'@'%' IDENTIFIED BY 'oagrram';" | mysql -u root
+    echo  "\e[1;32m----------------MARIADB UPDATE ROOT PASSWORD USER----------------\e[0m"
+    echo "ALTER USER 'root'@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -u root
+    echo  "\e[1;34m                --------------------------------\e[0m"
 
-echo "GRANT ALL PRIVILEGES ON *.* TO 'oagrram'@'%' WITH GRANT OPTION;" | mysql -u root
+fi 
 
-# CREATE WORDPRESS DATABASES
-
-echo "CREATE DATABASE wordpress_db;" | mysql -u root
-
-# CREATE WORDPRESS DATABASE USER
-
-echo "GRANT ALL ON wordpress_db.* TO 'otman'@'%' IDENTIFIED BY 'otman' WITH GRANT OPTION;" | mysql -u root
-
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
-service mysql restart
-
-# RUN SERVICE IN FORGROUND MODE
-
-echo -e "\e[1;32m Database Created. \e[0m"
-
-service mysql stop
+kill `cat /var/run/mysqld/mysqld.pid`
 
 mysqld_safe
